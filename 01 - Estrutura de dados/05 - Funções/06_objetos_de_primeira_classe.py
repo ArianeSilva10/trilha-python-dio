68a6eff2b6f3579b2292b8707581cc5f7764e8a8
Ariane <francisca.silva@alu.ufc.br>
2024-05-13 16:59:05 -0300

    Update funções Parte 02

--- 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
+++ 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
@@ -1,10 +1,22 @@
 def somar(a, b):
     return a + b
 
+def subtrair(a,b):
+    return a - b
+
+def test(a,b):
+    return a*2 + b*3
+
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação é : {resultado}")
 
 
-exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado(10, 10, somar)  # O resultado da operação é 20
+exibir_resultado(10,5,subtrair) # O resultado da operação é 5
+exibir_resultado(10,5,test) # O resultado da operação é 10 
+
+op = somar
+
+print(op(1,23))
