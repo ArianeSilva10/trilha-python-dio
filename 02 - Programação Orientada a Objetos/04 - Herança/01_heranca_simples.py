1b1ebaf9c707b721bb3ec8aa79a387deb708733b
Ariane <francisca.silva@alu.ufc.br>
2024-05-18 18:55:05 -0300

    Herança simples minha versão

--- 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
@@ -1,4 +1,4 @@
-class Veiculo:
+class veiculo:
     def __init__(self, cor, placa, numero_rodas):
         self.cor = cor
         self.placa = placa
@@ -7,31 +7,33 @@ class Veiculo:
     def ligar_motor(self):
         print("Ligando o motor")
 
-    def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+    def __str__(self) -> str:
+        return self.cor
 
-
-class Motocicleta(Veiculo):
+class Motocicleta(veiculo):
     pass
 
-
-class Carro(Veiculo):
+class Carro(veiculo):
     pass
 
-
-class Caminhao(Veiculo):
+class Caminhao(veiculo):
     def __init__(self, cor, placa, numero_rodas, carregado):
         super().__init__(cor, placa, numero_rodas)
         self.carregado = carregado
-
+        
     def esta_carregado(self):
         print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
 
 
 moto = Motocicleta("preta", "abc-1234", 2)
-carro = Carro("branco", "xde-0098", 4)
-caminhao = Caminhao("roxo", "gfd-8712", 8, True)
-
 print(moto)
-print(carro)
+
+moto.ligar_motor()
+
+carro = Carro("branco", "xde-0098", 4)
+carro.ligar_motor()
+
+caminhao = Caminhao("roxa", "gfd-8712", 8, True)
+caminhao.ligar_motor()
+caminhao.esta_carregado()
 print(caminhao)
