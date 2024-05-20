78a91843266873af21175779401c3b866a7472fd
Ariane <francisca.silva@alu.ufc.br>
2024-05-20 10:32:19 -0300

    herança multipla pratica

--- 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
@@ -1,34 +1,61 @@
 class Animal:
-    def __init__(self, nro_patas):
+    def __init__(self, nro_patas) -> None:
         self.nro_patas = nro_patas
 
-    def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {', '.join
+        ([f'{chave} = {valor}' for chave, valor in self.
+        __dict__.items()])}"
 
 class Mamifero(Animal):
-    def __init__(self, cor_pelo, **kw):
+    def __init__(self, cor_pelo, **kw) -> None:
         self.cor_pelo = cor_pelo
         super().__init__(**kw)
 
+    def __str__(self) -> str:
+        return 'Mamifero'
+
 
 class Ave(Animal):
-    def __init__(self, cor_bico, **kw):
+    def __init__(self, cor_bico, **kw) -> None:
         self.cor_bico = cor_bico
         super().__init__(**kw)
+
+    def __str__(self) -> str:
+        return 'ave 42'
+
 
 
 class Gato(Mamifero):
     pass
 
 
-class Ornitorrinco(Mamifero, Ave):
-    def __init__(self, cor_bico, cor_pelo, nro_patas):
-        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+class  Falar_mixim:
+    def falar(self):
+        return "oi estou falando"
+
+
+
+class Ornitorrinco(Mamifero, Ave, Falar_mixim):
+    def __init__(self, cor_bico, cor_pelo, nro_patas) -> None:
+        print(Ornitorrinco.__mro__)
+
+        print(Ornitorrinco.mro())
+
+        super().__init__(cor_pelo=cor_pelo,
+        cor_bico=cor_bico, nro_patas=nro_patas)
+
+    def __str__(self) -> str:
+        return "Ornitorrinco"
 
 
 gato = Gato(nro_patas=4, cor_pelo="Preto")
 print(gato)
 
-ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
+
+ornitorrinco = Ornitorrinco(nro_patas=2,
+cor_pelo="vermelho", cor_bico="laranja")
 print(ornitorrinco)
+print(ornitorrinco.falar())
