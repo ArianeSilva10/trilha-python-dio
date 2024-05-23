445a7d9ce95b30ddf71771a15dff5cf59ae416a0
Ariane <francisca.silva@alu.ufc.br>
2024-05-23 10:59:20 -0300

    metodos classe estatico

--- 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
+++ 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
@@ -1,20 +1,23 @@
 class Pessoa:
-    def __init__(self, nome, idade):
+    def __init__(self, nome=None, idade=None) -> None:
         self.nome = nome
         self.idade = idade
 
+
     @classmethod
-    def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+    def  criar_apartir_data_nascimento(cls, ano, mes, dia, nome):
         idade = 2022 - ano
         return cls(nome, idade)
-
+    
     @staticmethod
     def e_maior_idade(idade):
         return idade >= 18
 
+# p = Pessoa("Guilherme", 28)
+# print(p.nome, p.idade)
 
-p = Pessoa.criar_de_data_nascimento(1994, 3, 21, "Guilherme")
-print(p.nome, p.idade)
+p2 = Pessoa().criar_apartir_data_nascimento(1994, 3, 21, "Guilherme")
+print(p2.nome, p2.idade)
 
-print(Pessoa.e_maior_idade(18))
-print(Pessoa.e_maior_idade(8))
+print(Pessoa.e_maior_idade(28))
+print(Pessoa.e_maior_idade(10))
