78223233dccb3b45541237b2940c619ad26dcf16
Ariane <francisca.silva@alu.ufc.br>
2024-05-31 20:07:53 -0300

    1_datetime

--- 04 - Data e hora/1_datetime.py
+++ 04 - Data e hora/1_datetime.py
@@ -1,13 +1,17 @@
 from datetime import date, datetime, time
 
-data = date(2023, 7, 10)
+data = date(2023, 7, 10) # data
 print(data)
-print(date.today())
 
+print(date.today()) # data de hoje
 
-data_hora = datetime(2023, 7, 10)
+data_hora = datetime(2023, 7, 10, 10, 30, 20) # hora minuto e segundo
 print(data_hora)
-print(datetime.today())
+
+data_hora2 = datetime(2023, 7, 10) # hora minutos e segundos zerados por padrão
+print(data_hora2)
+
+print(datetime.today()) # data atual, hora minuto e segundos atual
 
 hora = time(10, 20, 0)
 print(hora)
