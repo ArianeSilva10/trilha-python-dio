f07b641715654f3d4f9810f6087f0c54963defd5
Ariane <francisca.silva@alu.ufc.br>
2024-06-01 20:09:19 -0300

    3_strftime_strptime

--- 04 - Data e hora/3_strftime_strptime.py
+++ 04 - Data e hora/3_strftime_strptime.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 data_hora_atual = datetime.now()
 data_hora_str = "2023-10-20 10:20"
-mascara_ptbr = "%d/%m/%Y %a"
+mascara_pt_br_DATA = "%d/%m/%Y" # data atual formatada em formato brasileiro
 mascara_en = "%Y-%m-%d %H:%M"
 
-print(data_hora_atual.strftime(mascara_ptbr))
+print(data_hora_atual.strftime(mascara_pt_br_DATA))
 
-data_convertida = datetime.strptime(data_hora_str, mascara_en)
-print(data_convertida)
-print(type(data_convertida))
+mascara_data_dia_semana = "%d/%m/%Y %a" #sugestão: estudar a documentação de datetime
+print(data_hora_atual.strftime(mascara_data_dia_semana))
+
+print(datetime.strptime(data_hora_str, mascara_en))
