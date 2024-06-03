9f5f284cfdcaea71ae8b56db4377c5b350e61964
Ariane <francisca.silva@alu.ufc.br>
2024-06-03 19:21:46 -0300

    5_timezone

--- 04 - Data e hora/4_pytz.py
+++ 04 - Data e hora/4_pytz.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+# pip install pytz
 
 import pytz
+from datetime import datetime
 
-data = datetime.now(pytz.timezone("Europe/Oslo"))
+data = datetime.now(pytz.timezone('Europe/Oslo'))
 data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
 
 print(data)
--- 04 - Data e hora/5_timezone.py
+++ 04 - Data e hora/5_timezone.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone, timedelta
 
-data_oslo = datetime.now(timezone(timedelta(hours=2)))
-data_sao_paulo = datetime.now(timezone(timedelta(hours=-3)))
+data_oslo = datetime.now(timezone(timedelta(hours=2), "OSL")) # nome secundário é opcional tipo "OSL"
+data_sao_paulo = datetime.now(timezone(timedelta(hours=3)))
 
 print(data_oslo)
 print(data_sao_paulo)
