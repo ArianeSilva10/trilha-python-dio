5d9f4e8568fafa63faf200f5ff15ed2a38ebbd99
Ariane <francisca.silva@alu.ufc.br>
2024-06-18 09:56:31 -0300

    refactor

--- 05 - Manipulação de arquivos/5_boas_praticas.py
+++ 05 - Manipulação de arquivos/5_boas_praticas.py
@@ -3,12 +3,12 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 
 try:
-    with open(ROOT_PATH / "1lorem.txt", "r") as arquivo:
+    with open(ROOT_PATH / "llorem.txt", "r") as arquivo:
         print(arquivo.read())
 except IOError as exc:
     print(f"Erro ao abrir o arquivo {exc}")
 
-
+# arquivo.close()
 # try:
 #     with open(ROOT_PATH / "arquivo-utf-8.txt", "w", encoding="utf-8") as arquivo:
 #         arquivo.write("Aprendendo a manipular arquivos utilizando Python.")
@@ -16,9 +16,9 @@ except IOError as exc:
 #     print(f"Erro ao abrir o arquivo {exc}")
 
 try:
-    with open(ROOT_PATH / "arquivo-utf-8.txt", "r", encoding="utf-8") as arquivo:
+    with open(ROOT_PATH / "arquivo-utf-8.txt", "r", encoding="ascii") as arquivo:
         print(arquivo.read())
 except IOError as exc:
-    print(f"Erro ao abrir o arquivo {exc}")
+    print(f"\nErro ao abrir o arquivo {exc}")
 except UnicodeDecodeError as exc:
-    print(exc)
+    print(f"\n{exc}")
